62d388a27b29d2d7c2ec6674134fb60fd86c3fcf
Hisham <hkherbouch@gmail.com>
2024-05-22 13:00:06 -0400

    First Commit

---
+++ robots/hisham/Hisham.java
@@ -0,0 +1,22 @@
+package hisham;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class Hisham extends Robot{
+    public void run(){
+       
+        while (true){
+            ahead(100);
+            turnLeft(45);
+            turnGunRight(90);
+            back(50);
+            turnRight(90);
+            turnGunLeft(45);
+
+        }
+    }
+    public void onScannedRobot(ScannedRobotEvent e){
+            fire(1);
+    }
+    
+}
